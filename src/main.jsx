f0110d3d2e6e30e0a47781d244e3162900d84283
azilRababe <azilrababe@gmail.com>
2023-09-16 17:52:29 +0100

    ...

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.scss'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 import Particles_bg from './components/Particles_bg.jsx'
 import AnimatedCursor from 'react-animated-cursor'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Particles_bg />
       <AnimatedCursor />
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )
--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.html'],
-  base: '/react-portfolio/',
-  build: {
-    outDir: 'dist',
-    assetsDir: '',
-    rollupOptions: {
-      output: {
-        publicPath: '/react-portfolio/',
-      },
-    },
-  },
+  base: '',
 })
