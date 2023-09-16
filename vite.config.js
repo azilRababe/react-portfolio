2d63dbf4ee1d962f970d71d6f475772444606526
azilRababe <azilrababe@gmail.com>
2023-09-16 17:00:22 +0100

    deployment ...

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.html'],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+  base: '/react-portfolio/',
+  build: {
+    outDir: 'dist',
+    assetsDir: '',
+    rollupOptions: {
+      output: {
+        publicPath: '/react-portfolio/',
       },
     },
   },
